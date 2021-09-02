6b64d7952b0cbebc5abaab74281d210fda9bd758
AlanPL <apyolt@gmail.com>
2021-09-02 14:43:39 -0500

    add access to mongodb uri

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DEV_DB, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect((process.env.MONGODB_URI || process.env.DEV_DB), {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
